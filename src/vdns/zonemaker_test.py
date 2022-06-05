f697b4f550d2ca119adedc67723eae7c2aea2813
Stefanos Harhalakis <v13@v13.gr>
2022-06-05 22:24:18 +0100

    Improve tests. Add test for glue records.

--- src/vdns/zonemaker_test.py
+++ src/vdns/zonemaker_test.py
@@ -10,6 +10,8 @@ import vdns.zoneparser
 
 from typing import Sequence, Union
 
+NeedLines = Sequence[Union[str, tuple[str, bool]]]
+
 
 class TestZoneMaker(unittest.TestCase):
 
@@ -19,8 +21,7 @@ class TestZoneMaker(unittest.TestCase):
         vdns.src.dynamic_testlib.init()
         vdns.db_testlib.add_test_data()
 
-    def _check_lines(self, lines: Sequence[str], needed_lines: Sequence[Union[str, tuple[str, bool]]],
-                     ignore_spaces: bool = True) -> None:
+    def _check_lines(self, lines: Sequence[str], needed_lines: NeedLines, ignore_spaces: bool = True) -> None:
         """Checks whether needed_lines are in lines.
 
         needed_lines is a list of either:
@@ -100,7 +101,7 @@ class TestZoneMaker(unittest.TestCase):
             elif forbidden:
                 self.assertNotIn(r.rr, forbidden, f"{r.rr} isn't an allowed RR: {line}")
 
-    def test_doit_v13_gr(self) -> None:
+    def test_v13_gr(self) -> None:
         domain = 'v13.gr'
         zm = vdns.zonemaker.ZoneMaker(domain, None)
         res = zm.doit(False, False)
@@ -111,7 +112,7 @@ class TestZoneMaker(unittest.TestCase):
         self._assert_allowed_rrs(lines, forbidden=('PTR',))
 
         # The order matters
-        needed_lines = [
+        needed_lines: NeedLines = [
             '1H IN NS ns1.dns.example.com.',
             '1H IN NS ns2.dns.example.com.',
 
@@ -122,21 +123,22 @@ class TestZoneMaker(unittest.TestCase):
             'IN AAAA 2001:db8:1::1',
 
             'dyn 5M IN NS ns1.example.com.',
-            'sub 5M IN NS ns1.example.com.',
-            'sub 5M IN NS ns2.example.com.',
+            'sub 5M IN NS ns1.sub.v13.gr.',
+            'sub 5M IN NS ns2.sub.v13.gr.',
+            'sub 5M IN NS ns3.example.com.',
 
             'host1 IN A 10.1.1.1',
-            'www IN CNAME host1',
+            ('www IN CNAME host1', True),
 
             'host3 15M IN A 10.1.1.3',
-            'IN AAAA 2001:db8:2c1:12::1',
+            ('IN AAAA 2001:db8:2c1:12::1', True),
 
             'ldap 30D IN CNAME host2.v13.gr.',
         ]
 
         self._check_lines(lines, needed_lines)
 
-    def test_doit_dyn_v13_gr(self) -> None:
+    def test_dyn_v13_gr(self) -> None:
         domain = 'dyn.v13.gr'
         vdns.src.dynamic_testlib.set_contents(textwrap.dedent('''
         $ORIGIN         dyn.v13.gr.
@@ -170,17 +172,17 @@ class TestZoneMaker(unittest.TestCase):
         self._assert_allowed_rrs(lines, forbidden=('PTR',))
 
         # The order matters
-        needed_lines = [
+        needed_lines: NeedLines = [
             '5M IN NS ns1.example.com.',
             '5M IN NS ns2.example.com.',
             'host1 IN A 10.1.1.1',       # The static of the dynamic
-            'IN A 10.9.1.1',            # The dynamic
+            ('IN A 10.9.1.1', True),     # The dynamic
             'host2 IN A 10.8.1.1',       # The fully static entry
         ]
 
         self._check_lines(lines, needed_lines)
 
-    def test_doit_reverse_10(self) -> None:
+    def test_reverse_10(self) -> None:
         domain = '10.in-addr.arpa'
         zm = vdns.zonemaker.ZoneMaker(domain, None)
         res = zm.doit(False, False)
@@ -199,7 +201,7 @@ class TestZoneMaker(unittest.TestCase):
         ]
         self._check_lines(lines, needed_lines)
 
-    def test_doit_reverse_2001(self) -> None:
+    def test_reverse_2001(self) -> None:
         domain = '8.b.d.0.1.0.0.2.ip6.arpa'
         zm = vdns.zonemaker.ZoneMaker(domain, None)
         res = zm.doit(False, False)
@@ -215,5 +217,20 @@ class TestZoneMaker(unittest.TestCase):
             '1.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.2.1.0.0.1.c.2.0 IN PTR host3.v13.gr.',
             '1.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.3.1.0.0.1.c.2.0 IN PTR host5.v13.gr.',
             '1.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.2.1.2.3.1.c.2.0 15M IN PTR host3.v13.gr.',
+        ]
+        self._check_lines(lines, needed_lines)
+
+    def test_glue(self) -> None:
+        domain = 'v13.gr'
+        zm = vdns.zonemaker.ZoneMaker(domain, None)
+        res = zm.doit(False, False)
+        logging.debug('Output for %s:\n%s', domain, res.zone)
+        lines = res.zone.splitlines()
+
+        needed_lines = [
+            'sub 5M IN NS ns1.sub.v13.gr.',
+            'sub 5M IN NS ns2.sub.v13.gr.',
+            'sub 5M IN NS ns3.example.com.',
+            'ns2.sub IN A 10.1.2.2',  # Glue record
         ]
         self._check_lines(lines, needed_lines)
