b1872c13755bbc609c2e8793725bb74f6233fb56
Stefanos Harhalakis <v13@v13.gr>
2022-09-20 02:03:49 +0100

    Update license years. Add Google copyright

--- src/vdns/util/config.py
+++ src/vdns/util/config.py
@@ -1,4 +1,20 @@
 # Config works as follows:
+#
+# Copyright (c) 2014-2016 Stefanos Harhalakis <v13@v13.gr>
+# Copyright (c) 2016-2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # - There are two configs: The global(util) config and the per-module
 #   config. Each one is an object with attributes. To access the config
 #   on should call get_config() from here. This will return a MergedConfig
